a51db03e3e17ea69749b8f340ec90c13addff5f9
耿良祥 <gengliangxiang@gengliangxiangdeMacBook-Pro.local>
2020-01-03 17:09:29 +0800

    删除

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -3,8 +3,6 @@ import { Controller } from 'egg';
 export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
-	// ctx.body = await ctx.service.test.sayHi('egg');
-	console.log(122)
     ctx.body = await ctx.service.test.sayHi('egg');
   }
 }
