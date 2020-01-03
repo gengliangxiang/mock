330aba8adea020681937408bd88eb213570e32b5
耿良祥 <gengliangxiang@gengliangxiangdeMacBook-Pro.local>
2020-01-03 17:01:48 +0800

    w

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -2,7 +2,10 @@ import { Controller } from 'egg';
 
 export default class HomeController extends Controller {
   public async index() {
-    const { ctx } = this;
-    ctx.body = await ctx.service.test.sayHi('egg');
+	const { ctx } = this;
+	const e = 'erondcds';
+	console.log(e);
+	ctx.body = await ctx.service.test.sayHi('egg');
+	console.log(2342)
   }
 }
